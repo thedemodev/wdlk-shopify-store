e85a0a15f7d2042e8c730396355d9433af252280
Maximilian Heinz <maximilian.heinz@sinnerschrader.com>
2015-08-14 14:06:08 +0200

    implemented first version of build tasks

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,11 @@
-//= require_tree .
+require('./ext/fastclick.min.js');
+require('./ext/handlebars.min.js');
+require('./ext/modernizr.min.js');
+require('./ext/jquery-1.11.2.min.js');
+require('./ext/respond.min.js');
+
+var example = require('./modules/example.js');
+
+$(document).ready(function() {
+    example();
+});
---
+++ src/scripts/modules/example.js
@@ -0,0 +1,5 @@
+var example = function () {
+    console.log('example');
+}
+
+module.exports = example;
