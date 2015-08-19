d9cacbcba7587a3efd24f6a6344466f3b0ab2c5b
Mauricio Palma <info@woodlike.de>
2015-08-19 09:37:56 +0200

    (WIP: modularizing timbers one scss file)

--- tasks/config/sass.js
+++ tasks/config/sass.js
@@ -8,6 +8,7 @@ function scss (cb) {
     var srcFile   = path.resolve( __dirname, '..', '..', 'src/scss/index.scss');
     var buildFile = path.resolve( __dirname, '..', '..', 'assets/index.scss');
     var libsPath  = path.resolve( __dirname, '..', '..', 'src/scss/libs');
+    var bourbonPath = path.resolve( __dirname, '..', '..', 'node_modules/bourbon/app/assets/stylesheets/');
 
     sass.render({
         file: srcFile,
@@ -19,13 +20,13 @@ function scss (cb) {
         if (err) {
             console.log('Error status:', err.status);
             console.log('Error column:', err.column, 'Error Line:', err.line);
+            console.log('Error found in the file:', err.file );
             console.log(err.message);
         } else {
                 var results = postcss([autoprefixer({ browsers: ['last 2 versions'] })]).process(result.css);
 
                 fs.writeFile(buildFile, results);
-
-                console.log(results.css.toString());
+                // console.log(results.css.toString());
                 console.log(results.messages.toString());
                 console.log(results);
         }
