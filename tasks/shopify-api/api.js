814da9c55da974e8a06f3f264d076393be486cde
Mauricio Palma <info@woodlike.de>
2016-02-07 20:19:06 +0100

    feature/product-detail: implemeneted communication with the shopify api

---
+++ tasks/shopify-api/api.js
@@ -0,0 +1,20 @@
+import config from './config';
+import metaFields from './data/product.json'
+
+export default function api () {
+    const SHOPIFY = config();
+    console.log(metaFields);
+
+    SHOPIFY.post('/admin/products/414034396/metafields.json', metaFields,  (err, data, headers) => {
+        console.log(data); // Data contains product json information
+        console.log(headers); // Headers returned from request
+    });
+
+    // SHOPIFY.delete('/admin/products/414029792/metafields/14571187524.json', metaFields,  (err, data, headers) => {
+    //     console.log(err);
+    //     console.log(data.STATUS); // Data contains product json information
+    //     // console.log(headers); // Headers returned from request
+    // });
+}
+
+api();
