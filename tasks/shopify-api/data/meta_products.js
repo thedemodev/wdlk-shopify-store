fde5ca6c545e2961268aa973d6331d0dc78a6c12
Mauricio Palma <info@woodlike.de>
2016-02-08 09:02:59 +0100

    feature/product-detail: move the structure of the metafields data

---
+++ tasks/shopify-api/data/meta_products.js
@@ -0,0 +1,9 @@
+export let material = {
+  "metafield": {
+    "namespace": "swimwear",
+    "key": "Material",
+    "value": "Features text of the bikinis",
+    "description": "Provides detail information about the material features and properties",
+    "value_type": "string"
+  }
+}
