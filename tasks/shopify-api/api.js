01813c3d3343b62949345f844a23534fac030116
Mauricio Palma <info@woodlike.de>
2016-02-09 00:29:06 +0100

    (WIP)feature/product-detail: get the product ids

--- tasks/shopify-api/api.js
+++ tasks/shopify-api/api.js
@@ -1,11 +1,13 @@
-import request from '.request';
-import {material} from './data/meta_products';
+// import request from './request';
+// import {material} from './data/meta_products';
 
+// POST the global store metafield data for the products material
+// request(post, '/admin/metafields.json', material);
+// request(config(), 'post', '/admin/metafields.json', material);
 
+// request('GET', 'admin/metafields.json');
 
-// export function getProductIds
-
-// Get the global store metafield data for the product
-request(get, '/admin/metafields.json', material);
-
-// Get the ids
+// config().get('/admin/metafields.json', (err, data, headers) => {
+//     console.log(data); // Data contains product json information
+//     console.log(headers); // Headers returned from request
+// });
---
+++ tasks/shopify-api/products-ids.js
@@ -0,0 +1,11 @@
+import log from '../log';
+import config from './config';
+
+export default function getProductsId () {
+    const SHOPIFY = config();
+    const IDS = [];
+
+    console.log(SHOPIFY);
+}
+
+getProductsId();
