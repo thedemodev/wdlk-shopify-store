2ee4b5ad6daa2a7bf88195b38d10e69b4bfe713a
Mauricio Palma <info@woodlike.de>
2016-02-08 19:06:18 +0100

    feature/product-detail: divide the api communication in smaller actions

--- tasks/shopify-api/api.js
+++ tasks/shopify-api/api.js
@@ -1,20 +1,11 @@
-import config from './config';
-import {material} from './data/meta_products.js'
+import request from '.request';
+import {material} from './data/meta_products';
 
-export default function api () {
-    const SHOPIFY = config();
-    console.log(material);
 
-    SHOPIFY.post('/admin/metafields.json', metaFields,  (err, data, headers) => {
-        console.log(data); // Data contains product json information
-        console.log(headers); // Headers returned from request
-    });
 
-    // SHOPIFY.delete('/admin/products/414029792/metafields/14571187524.json', metaFields,  (err, data, headers) => {
-    //     console.log(err);
-    //     console.log(data.STATUS); // Data contains product json information
-    //     // console.log(headers); // Headers returned from request
-    // });
-}
+// export function getProductIds
 
-api();
+// Get the global store metafield data for the product
+request(get, '/admin/metafields.json', material);
+
+// Get the ids
