60fce24cc451549fa22aa0ed03ff225b0bd1949d
Mauricio Palma <info@woodlike.de>
2016-02-13 19:36:15 +0100

    feature/product-detail: included metafield delete mechanism

---
+++ tasks/shopify-api/utils/delete-metafields.js
@@ -0,0 +1,29 @@
+import shopifyAPI from 'shopify-node-api';
+import {readFile} from 'fs';
+import {CONFIG} from '../config';
+import log from '../../log';
+
+export default function deleteMeta (dataType, id, metafieldId) {
+    const SHOPIFY = new shopifyAPI(CONFIG);
+    const TIME = new Date();
+    let endPoint;
+
+    if (dataType === 'store') {
+        endPoint = `/admin/metafields/${id}.json`;
+
+    } else {
+        endPoint = `/admin/${dataType}/${id}/metafields/${metafieldId}.json`;
+    }
+
+    SHOPIFY.delete(endPoint, (err, data) => {
+        if (err) {
+            console.error(log.error(`${TIME} [Global Metafields DELETE request]: `), err);
+            return;
+        }
+        console.log(log.info(`${TIME} [Global Metafields DELETE request]:`), 'was successful');
+    });
+
+
+}
+
+// deleteMeta('products', 414029792, 14829089284);
