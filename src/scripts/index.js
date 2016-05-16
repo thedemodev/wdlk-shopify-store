ddbd08bbd69df6d8202ff90f77385c1967571afb
Karsten Buckstegge <k.buckstegge@gmail.com>
2016-05-16 12:46:43 +0200

    add cookieNotification functionality

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,9 @@
 import rAf from './polyfills/rAf';
 import stickyNavigation from './modules/sticky-navigation';
 import smoothScrolling from './modules/smooth-scrolling';
+import cookieNotification from './modules/cookie-notification';
 
 rAf();
 smoothScrolling();
 stickyNavigation();
+cookieNotification();
---
+++ src/scripts/modules/cookie-notification.js
@@ -0,0 +1,24 @@
+const notification = document.querySelector('.js-cookie-notification');
+const closeButton = document.querySelector('.js-cookie-notification-close');
+const localStorage = window.localStorage;
+
+const notificationHidden = () => {
+    return localStorage.getItem('hideCookieNotification');
+}
+
+const hideNotification = () => {
+    localStorage.setItem('hideCookieNotification', true);
+    notification.style.display = 'none';
+}
+
+export default () => {
+    if(notificationHidden()) {
+        return;
+    }
+
+    notification.style.display = 'block';
+    const removeCloseButtenClickHandler = closeButton.addEventListener('click', () => {
+        hideNotification();
+        closeButton.removeEventListener('click', removeCloseButtenClickHandler);
+    });
+};
