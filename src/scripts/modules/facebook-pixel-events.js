5c7a0f0e552ddb9cc3514343f5348f1506a90189
Mauricio Palma <info@woodlike.de>
2016-11-27 14:56:20 +0100

    Include facebook tracking for the newsletter

--- src/scripts/modules/facebook-pixel-events.js
+++ src/scripts/modules/facebook-pixel-events.js
@@ -1,16 +1,24 @@
 export default function fbqEvents () {
+	let signUpBtn = document.querySelector('.js_fbqNewsLead');
 	let addToCartBtn = document.querySelector('.js_fbqAddToCart');
-	if (!addToCartBtn) {
-		return;
-	}
-	addToCartBtn.addEventListener('click', () => {
-		fbq('track', 'AddToCart', {
-			content_name: addToCartBtn.dataset.name,
-			content_ids: [addToCartBtn.dataset.id],
-			content_type: 'product',
-			value: parseInt(addToCartBtn.dataset.value)
+
+	if (addToCartBtn) {
+		addToCartBtn.addEventListener('click', () => {
+			fbq('track', 'AddToCart', {
+				content_name: addToCartBtn.dataset.name,
+				content_ids: [addToCartBtn.dataset.id],
+				content_type: 'product',
+				value: parseInt(addToCartBtn.dataset.value)
+			});
 		});
-		console.log('Facebook AddToCart event was succesfully initiated');
-	});
+	}
+
+	if (signUpBtn) {
+		signUpBtn.addEventListener('click', () => {
+			fbq('track', 'Lead');
+		});
+	}
+
+	return;
 }
 
