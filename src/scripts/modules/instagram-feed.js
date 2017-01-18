2913c3bc09ee1cfeffa807248096d2e9cc8980ff
Mauricio Palma <info@woodlike.de>
2017-01-18 15:27:40 +0100

    Improved the js implementation of the instagram feed

--- src/scripts/modules/instagram-feed.js
+++ src/scripts/modules/instagram-feed.js
@@ -2,11 +2,11 @@ import jsonp from 'jsonp';
 import {access} from '../../../tasks/instagram/access';
 
 export default function generateFeed () {
-	const nodeList = document.querySelector('.js_instagramFeed');
-    if (!nodeList) return;
-    if (typeof Promise === undefined) return;
+	const node = document.querySelector('.js_instagramFeed');
+	if (!node) return;
+	if (typeof Promise === undefined) return;
 
-    const feedLimit = 11;
+	const feedLimit = 11;
 	const instaURL = `https://api.instagram.com/v1/users/${access['id']}/media/recent/?access_token=${access['token']}&callback=callback`;
 
 	const getMediaFeed = url => {
@@ -24,22 +24,53 @@ export default function generateFeed () {
 
 	const limitArr = value => value <= feedLimit;
 	const filteredArr = arr => arr.filter((el, i) => limitArr(i));
-	const generateImgs = arr => {
-		arr.forEach(el => {
-			let listItem = document.createElement('li');
-			let image = new Image();
+	const createNode = (el, classname, attr, attrValue, id) => {
+		if (el === 'img') {
+			el = new Image();
+		} else {
+			el = document.createElement(`${el}`);
+		}
 
-			listItem.className = 'Media-item';
-			image.src = `${el.images.standard_resolution.url}`;
-			listItem.appendChild(image);
-			nodeList.insertBefore(listItem, null);
-		})
+		el.className = `${classname}`;
+
+		if (attr) {
+ 			el[attr] = `${attrValue}`;
+		}
+
+		if (id) {
+			el.id =`${id}`;
+		}
+
+		return el;
 	};
+
+	const createFeed = arr => {
+		const foo = arr.map(el => {
+			let html = `
+				<li class="Feed-item">
+					<img class="Feed-img" src="${el.images.standard_resolution.url}" />
+					<input class="Lightbox-state" type="checkbox" id="lightbox" />
+					<div class="Lightbox-shim">
+						<figure class="Lightbox-content js_image">
+							<img class="Feed-img" src="${el.images.standard_resolution.url}" />
+							<figcaption class="Feed-caption js_caption">
+								${el.caption.text}
+							</figcaption>
+						</figure>
+					</div>
+				</li>
+			`;
+			return html;
+		});
+
+		node.innerHTML = foo.join();
+	};
+
 
 
 	getMediaFeed(instaURL).then(res => {
 		const feedData = filteredArr(res.data);
-		generateImgs(feedData);
+		createFeed(feedData);
 	});
 
 }
