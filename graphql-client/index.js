cd9868f78bf0c4df4228c9cabba4f0f389387477
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2017-05-04 14:03:39 +0200

    feat($graphql): include the graphql fetch

--- graphql-client/index.js
+++ graphql-client/index.js
@@ -1,5 +1,32 @@
 // @flow
+const fetch = require('graphql-fetch')('https://wdkl-ocean.myshopify.com/api/graphql');
+const config = require('./config');
 
-import {ApolloClient} from 'react-apollo';
+const queryVars = {};
+
+const query = `
+{
+  shop {
+    products(first: 30, query: "title") {
+      edges {
+        node {
+          title
+          id
+        }
+      }
+    }
+  }
+}
+`;
+
+const opts = {
+  headers: config
+};
 
 
+fetch(query, queryVars, opts).then(results => {
+  if (results.errors) {
+    console.log('@@@@@', results.errors);
+  }
+  console.log(results);
+});
