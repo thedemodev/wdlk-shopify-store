45b0ea1bd6f7ff4b22096375274ad46a47061572
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2017-10-02 13:30:28 +0200

    fix($theme-component): setProperty only if the theme element is in the document

--- packages/wdlk-store/src/scripts/modules/theme.js
+++ packages/wdlk-store/src/scripts/modules/theme.js
@@ -1,4 +1,6 @@
 export default function setTheme () {
   const el = document.querySelector('.withTheme');
-  el.style.setProperty('--theme-invert', 'unset');
+  if (el) {
+    el.style.setProperty('--theme-invert', 'unset');
+  }
 }
