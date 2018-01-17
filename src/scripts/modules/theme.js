7836bd5431c01629b57a610f38a0ecbf16da27a9
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2018-01-18 00:00:14 +0100

    chore($(webpack)): include happy pack for multiple threads

--- src/scripts/modules/theme.js
+++ src/scripts/modules/theme.js
@@ -1,4 +1,4 @@
-export default function setTheme () {
+export default function setTheme() {
   const el = document.querySelector('.withTheme');
   if (el) {
     el.style.setProperty('--theme-invert', 'unset');
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const { resolve } = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const HappyPack = require('happypack');
+
+// Ensure `postcss` key is extracted
+HappyPack.SERIALIZABLE_OPTIONS = HappyPack.SERIALIZABLE_OPTIONS.concat(['postcss'])
+
+module.exports = {
+  entry: './src/scripts/index',
+  output: {
+    path: resolve('./src/assets/'),
+    filename: 'index.js.liquid'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'happypack/loader?id=ts'
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('happypack/loader?id=styles')
+      }
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin({ filename: resolve('/index.css.liquid')}),
+    new ForkTsCheckerWebpackPlugin({ checkSyntacticErrors: true }),
+
+    new HappyPack({
+      id: 'styles',
+      loaders: [ 'css-loader', 'postcss-loader', 'sass-loader']
+    }),
+
+    new HappyPack({
+      id: 'ts',
+      loaders: [
+        {
+          path: 'ts-loader',
+          query: { happyPackMode: true }
+        }
+      ],
+      threads: 4
+    })
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  watch: true
+}
