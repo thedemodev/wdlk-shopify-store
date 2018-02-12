9b44584ec660667468338aafc2216df6151f57a5
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2018-02-13 00:22:50 +0100

    chore(modules): include missing typings

--- src/scripts/modules/tracking-project-cece.ts
+++ src/scripts/modules/tracking-project-cece.ts
@@ -1,23 +1,23 @@
-export default function trackingCece () {
-  var x = document.referrer; function set_cece_cookie() {
-    var expiration = new Date();
-    expiration.setTime(expiration.getTime() + (30*24*60*60*1000));
-    document.cookie = "refpartner=projce; expires=" + expiration.toUTCString() + "; path=/";
+export default function trackingCece(): void {
+  const x = document.referrer;
+  function set_cece_cookie(): void {
+    const expiration = new Date();
+    expiration.setTime(expiration.getTime() + (30 * 24 * 60 * 60 * 1000));
+    document.cookie = `refpartner=projce; expires=${expiration.toUTCString()}; path=/`;
   }
 
   if (x.indexOf('projectcece.') > -1) {
     set_cece_cookie();
-    if (window.sessionStorage){
-      sessionStorage.setItem("refpartner", "projce");
+    if (window.sessionStorage) {
+      sessionStorage.setItem('refpartner', 'projce');
     }
   }
 
-  if (window.sessionStorage){
-      if (sessionStorage.getItem("refpartner")) {
+  if (window.sessionStorage) {
+      if (sessionStorage.getItem('refpartner')) {
         if (document.cookie.indexOf('refpartner=projce') < 0) {
           set_cece_cookie();
           }
       }
   }
 }
-
