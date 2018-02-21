2b3281f1ca706c033fbd2359656508da7ff0225e
Mauricio Palma <mauricio.palma@sinnerschrader.com>
2018-02-22 00:08:28 +0100

    feat(fade-through): include fade-in-out logic

--- src/scripts/modules/fade-through.ts
+++ src/scripts/modules/fade-through.ts
@@ -1,7 +1,24 @@
 const fadeThrough = () => {
   const el: HTMLElement = document.querySelector('.js_fadeThrough');
+  if (!el) {
+    return;
+  }
+  const btnList: NodeListOf<Element> = document.querySelectorAll(
+    '.js_fadeThrough-btn'
+  );
+  if (!btnList) {
+    return;
+  }
+  let isVisible = false;
 
-  console.log('this is working', el);
+  const handleClick = (e: Event): void => {
+    isVisible = !isVisible;
+    el.style.setProperty('--is-visible', `${isVisible ? 1 : 0}`);
+  };
+
+  [...btnList].forEach((btn: HTMLElement) => {
+    btn.addEventListener('click', handleClick);
+  });
 };
 
 export default fadeThrough;
