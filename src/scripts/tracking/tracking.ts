4ba4b7914558c6471f3b00e6cf52badc5544bf23
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2018-05-24 19:35:24 +0200

    refactor(tracking): new implementation of the tracking manager (#24)
    * fix(notification): remove notification until fixed
    
    * chore(test): remove jest test coverage
    
    * fix(tracking): include facebook and google script manually
    
    * feat(tracking): include tracking manager unit test
    
    * fix(tracking): new implementation of facebook opt out pixel
    
    * fix(tracking): include new implementation of google opt out
    
    * fix(payment): remove amazon payment
    
    * chore(versioning): version bump

--- src/scripts/tracking/tracking.ts
+++ src/scripts/tracking/tracking.ts
@@ -1,94 +1,79 @@
-import facebookPixel from './facebook-pixel';
-import googleAnalytics from './google-analytics';
-export type trackingStatusTypes = 'fbIsNotTracking' | 'gaIsNotTracking';
 export type vendorTypes = 'facebook' | 'google';
+export interface TrackingProps {
+  facebook: boolean;
+  google: boolean;
+  disableTracking(vendor: string): void;
+}
 
-export default function tracking(): void {
-  const fbId = 'js_facebook-pixel';
-  const gaId = 'js_ga-tracking';
-  const fbOptOutEl = document.querySelector('.js_fb-opt-out');
-  const gaOptOutEl = document.querySelector('.js_ga-opt-out');
+export function TrackingManagerFactory(): TrackingProps {
+  return {
+    facebook: true,
+    google: true,
+    disableTracking(vendor: string): void {
+      const keys = Object.keys(this);
 
-  const addScript = (script: string) => {
-    if (typeof script === 'string') {
-      document.head.insertAdjacentHTML('beforeend', script);
+      keys.forEach(key => {
+        if (key === vendor) {
+          this[vendor] = false;
+        }
+      });
     }
   };
+}
 
-  const removeScript = (
-    node: NodeListOf<Element>,
-    status: trackingStatusTypes
-  ) => {
-    const list = [].slice.call(node);
-    list.forEach((element: Element) => {
-      if (element) {
-        element.parentElement.removeChild(element);
+export default function tracking(): void {
+  const fbOptOutList = document.querySelectorAll('.js_fb-opt-out');
+  const gaOptOutList = document.querySelectorAll('.js_ga-opt-out');
+  const trackingManager = TrackingManagerFactory();
+
+  const handleOptOut = (vendor: vendorTypes): void => {
+    const nodeList = document.querySelectorAll(`.js_${vendor}-tracking`);
+    const scriptList = [].slice.call(nodeList);
+    if (JSON.parse(localStorage.getItem(vendor)) === true) {
+      trackingManager.disableTracking(vendor);
+      localStorage.setItem(vendor, `${trackingManager[vendor]}`);
+      console.log(
+        JSON.parse(localStorage.getItem(vendor)) === true,
+        'in the the handle opt out'
+      );
+    }
+    scriptList.forEach((script: Element) => {
+      if (script) {
+        script.parentElement.removeChild(script);
       }
     });
   };
 
-  const setStatus = (status: trackingStatusTypes) => {
-    if (localStorage.getItem(status) === 'true') {
-      return;
-    } else {
-      localStorage.setItem(status, 'true');
+  const setValues = (): void => {
+    if (localStorage.getItem('facebook') === null) {
+      localStorage.setItem('facebook', `${trackingManager.facebook}`);
     }
-    console.log('set status to', status);
-  };
-
-  if (localStorage.getItem('fbIsNotTracking') === 'true') {
-    removeScript(document.querySelectorAll(`.${fbId}`), 'fbIsNotTracking');
-  }
-
-  if (localStorage.getItem('fbIsNotTracking') === null) {
-    addScript(facebookPixel);
-  }
-
-  if (localStorage.getItem('gaIsNotTracking') === 'true') {
-    removeScript(document.querySelectorAll(`.${gaId}`), 'gaIsNotTracking');
-  }
-
-  if (localStorage.getItem('gaIsNotTracking') === null) {
-    addScript(googleAnalytics);
-  }
-
-  const handleOptOut = (node: Element, vendor: vendorTypes): void => {
-    if (node) {
-      switch (vendor) {
-        case 'facebook':
-          const removeFBTracking = () => {
-            setStatus('fbIsNotTracking');
-            removeScript(
-              document.querySelectorAll(`.${fbId}`),
-              'fbIsNotTracking'
-            );
-            alert('Facebook tracking has been deactivated');
-          };
-          node.addEventListener('click', removeFBTracking);
-          break;
-        case 'google':
-          const removeGATracking = () => {
-            setStatus('gaIsNotTracking');
-            removeScript(
-              document.querySelectorAll(`.${gaId}`),
-              'gaIsNotTracking'
-            );
-            const gaProperty = 'UA-79543949-2';
-            const disableStr = 'ga-disable-' + gaProperty;
-
-            document.cookie =
-              disableStr +
-              '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
-            window[disableStr] = true;
-            alert('Google Analytics tracking has been deactivated');
-          };
-          node.addEventListener('click', removeGATracking);
-      }
+    if (JSON.parse(localStorage.getItem('facebook')) === false) {
+      handleOptOut('facebook');
+    }
+    if (localStorage.getItem('google') === null) {
+      localStorage.setItem('google', `${trackingManager.google}`);
+    }
+    if (JSON.parse(localStorage.getItem('google')) === false) {
+      handleOptOut('google');
     }
   };
 
-  handleOptOut(fbOptOutEl, 'facebook');
-  handleOptOut(gaOptOutEl, 'google');
+  setValues();
+
+  [].slice.call(fbOptOutList).forEach((el: Element) => {
+    el.addEventListener('click', () => {
+      handleOptOut('facebook');
+      alert('You deactivated the Facebook tracking pixel!');
+    });
+  });
+
+  [].slice.call(gaOptOutList).forEach((el: Element) => {
+    el.addEventListener('click', () => {
+      handleOptOut('google');
+      alert('You deactivated the Google tracking pixel!');
+    });
+  });
 }
 
 tracking();
