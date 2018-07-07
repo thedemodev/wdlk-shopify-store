4e413449170d187e66299d202f556b65a0b233bb
Mauricio Palma <mauricio.palma@sinnerschrader.com>
2018-07-07 23:28:47 +0200

    feat(expander): include event handling

--- src/scripts/modules/expander.ts
+++ src/scripts/modules/expander.ts
@@ -1,13 +1,9 @@
-import { Observable, Observer, fromEvent } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
 import { AspectRatio } from './picture-aspect-ratio';
 import { mediaQuery, BreakPoint } from './match-media';
 import { ViewBox } from './viewbox';
 
-// `0 0 ${BreakPoint.S} ${
-//   isExpanded ? BreakPoint.S * AspectRatio.S : viewportWidth / 2
-// }`
-
-const expanderViewBox = {
+const viewBox = {
   S: new ViewBox({
     minX: 0,
     minY: 0,
@@ -36,42 +32,109 @@ const expanderViewBox = {
 
 export const viewBoxStream = Observable.create((observer: Observer<string>) => {
   if (mediaQuery.S.matches) {
-    observer.next(expanderViewBox.S.getValues());
+    observer.next(viewBox.S.getValues());
   }
   if (mediaQuery.M.matches) {
-    observer.next(expanderViewBox.M.getValues());
+    observer.next(viewBox.M.getValues());
   }
   if (mediaQuery.L.matches) {
-    observer.next(expanderViewBox.L.getValues());
+    observer.next(viewBox.L.getValues());
   }
   if (mediaQuery.XL.matches) {
-    observer.next(expanderViewBox.XL.getValues());
+    observer.next(viewBox.XL.getValues());
   }
 });
 
-export function setViewBox(nodeList: NodeListOf<Element>): void {
-  if (nodeList) {
-    [...nodeList].forEach((node: HTMLElement) => {
-      viewBoxStream.subscribe((viewbox: string) => {
-        node.setAttribute('viewBox', `${viewbox}`);
-      });
+function setViewBox(node: SVGElement): void {
+  if (node) {
+    viewBoxStream.subscribe((viewbox: string) => {
+      node.setAttribute('viewBox', `${viewbox}`);
     });
   }
 }
 
-export function onResize(nodeList: NodeListOf<Element>): void {
-  window.addEventListener('resize', () => setViewBox(nodeList), {
-    passive: true
+let isCollapsedFlag = false;
+
+function handleClick(e: MouseEvent): void {
+  const target = e.currentTarget as SVGElement;
+  isCollapsedFlag = !isCollapsedFlag;
+
+  viewBoxStream.subscribe((viewbox: string) => {
+    initExpander(target, isCollapsedFlag);
+    target.setAttribute('viewBox', `${viewbox}`);
   });
 }
 
+export function initExpander(
+  node: SVGElement,
+  isCollapsed: boolean = true
+): void {
+  const viewPortHeight = window.innerHeight;
+  const viewPortWidth = window.innerWidth;
+  const stickyHeaderHeight: number = (document.querySelector(
+    '.js_sticky-nav'
+  ) as HTMLElement).offsetHeight;
+  const height = {
+    S: {
+      collapsed: viewPortHeight / 3 - stickyHeaderHeight / 3,
+      expanded: BreakPoint.S * AspectRatio.S
+    },
+    M: {
+      collapsed: viewPortHeight / 3 - stickyHeaderHeight / 3,
+      expanded: BreakPoint.M * AspectRatio.M
+    },
+    L: {
+      collapsed: viewPortWidth / 3,
+      expanded: BreakPoint.L * AspectRatio.L
+    },
+    XL: {
+      collapsed: viewPortWidth / 3,
+      expanded: BreakPoint.XL * AspectRatio.L
+    }
+  };
+
+  if (node) {
+    if (mediaQuery.S.matches) {
+      viewBox.S.setHeight(isCollapsed ? height.S.collapsed : height.S.expanded);
+    }
+    if (mediaQuery.M.matches) {
+      viewBox.M.setHeight(isCollapsed ? height.M.collapsed : height.M.expanded);
+    }
+    if (mediaQuery.L.matches) {
+      viewBox.M.setHeight(isCollapsed ? height.L.collapsed : height.L.expanded);
+    }
+    if (mediaQuery.XL.matches) {
+      viewBox.M.setHeight(
+        isCollapsed ? height.XL.collapsed : height.XL.expanded
+      );
+    }
+  }
+}
+
 export default function expander(): void {
-  const expanderList: NodeListOf<Element> = document.querySelectorAll(
+  const expanderList: NodeListOf<SVGElement> = document.querySelectorAll(
     '.js_expander'
   );
 
   if (expanderList) {
-    setViewBox(expanderList);
-    window.requestAnimationFrame(() => onResize(expanderList));
+    [...expanderList].forEach((node: SVGElement) => {
+      initExpander(node);
+      setViewBox(node);
+      node.addEventListener('click', handleClick);
+    });
+    window.requestAnimationFrame(() => {
+      window.addEventListener(
+        'resize',
+        e => {
+          [...expanderList].forEach((node: SVGElement) => {
+            initExpander(node);
+            setViewBox(node);
+          });
+        },
+        {
+          passive: true
+        }
+      );
+    });
   }
 }
