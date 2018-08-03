f9b51c96d7b9e733878dde0a147e0dccfc4b95f4
Mauricio <mauricio.palma@sinnerschrader.com>
2018-08-03 21:05:20 +0200

    refactor(expander): removed unnecessary steps

--- src/scripts/modules/expander.ts
+++ src/scripts/modules/expander.ts
@@ -4,77 +4,44 @@ import { mediaQuery, BreakPoint } from './match-media';
 import { ViewBox } from './viewbox';
 import { ViewBoxDimension } from './viewbox-dimension';
 
-const viewBox = {
-  S: new ViewBox({
-    minX: 0,
-    minY: 0,
-    width: BreakPoint.S,
-    height: BreakPoint.S * AspectRatio.S
-  }),
-  M: new ViewBox({
-    minX: 0,
-    minY: 0,
-    width: BreakPoint.M,
-    height: BreakPoint.M * AspectRatio.M
-  }),
-  L: new ViewBox({
-    minX: 0,
-    minY: 0,
-    width: BreakPoint.L,
-    height: BreakPoint.L * AspectRatio.L
-  }),
-  XL: new ViewBox({
-    minX: 0,
-    minY: 0,
-    width: BreakPoint.XL,
-    height: BreakPoint.XL * AspectRatio.L
-  })
-};
-
-export const viewBoxStream = Observable.create((observer: Observer<string>) => {
-  if (mediaQuery.S.matches) {
-    observer.next(viewBox.S.getValues());
-  }
-  if (mediaQuery.M.matches) {
-    observer.next(viewBox.M.getValues());
-  }
-  if (mediaQuery.L.matches) {
-    observer.next(viewBox.L.getValues());
-  }
-  if (mediaQuery.XL.matches) {
-    observer.next(viewBox.XL.getValues());
-  }
-});
-
-function setViewBox(node: SVGElement): void {
-  if (node) {
-    viewBoxStream.subscribe((viewbox: string) => {
-      node.setAttribute('viewBox', `${viewbox}`);
-    });
-  }
-}
-
-let isCollapsedFlag = false;
-
-function handleClick(e: MouseEvent): void {
-  const target = e.currentTarget as SVGElement;
-  isCollapsedFlag = !isCollapsedFlag;
-
-  viewBoxStream.subscribe((viewbox: string) => {
-    initExpander(target, isCollapsedFlag);
-    target.setAttribute('viewBox', `${viewbox}`);
-  });
-}
-
-export function initExpander(
-  node: SVGElement,
-  isCollapsed: boolean = true
-): void {
+export default function expander(): void {
   const viewPortHeight = window.innerHeight;
   const viewPortWidth = window.innerWidth;
   const stickyHeaderHeight: number = (document.querySelector(
     '.js_sticky-nav'
   ) as HTMLElement).offsetHeight;
+  const expanderList: NodeListOf<SVGElement> = document.querySelectorAll(
+    '.js_expander'
+  );
+  let isCollapsed = true;
+
+  const viewBox = {
+    S: new ViewBox({
+      minX: 0,
+      minY: 0,
+      width: BreakPoint.S,
+      height: BreakPoint.S * AspectRatio.S
+    }),
+    M: new ViewBox({
+      minX: 0,
+      minY: 0,
+      width: BreakPoint.M,
+      height: BreakPoint.M * AspectRatio.M
+    }),
+    L: new ViewBox({
+      minX: 0,
+      minY: 0,
+      width: BreakPoint.L,
+      height: BreakPoint.L * AspectRatio.L
+    }),
+    XL: new ViewBox({
+      minX: 0,
+      minY: 0,
+      width: BreakPoint.XL,
+      height: BreakPoint.XL * AspectRatio.L
+    })
+  };
+
   const viewBoxDimensions = {
     S: ViewBoxDimension({
       collapsed: viewPortHeight / 3 - stickyHeaderHeight / 3,
@@ -94,13 +61,14 @@ export function initExpander(
     })
   };
 
-  if (node) {
+  const viewBoxStream = Observable.create((observer: Observer<string>) => {
     if (mediaQuery.S.matches) {
       viewBox.S.setHeight(
         isCollapsed
           ? viewBoxDimensions.S.collapsed
           : viewBoxDimensions.S.expanded
       );
+      observer.next(viewBox.S.getValues());
     }
     if (mediaQuery.M.matches) {
       viewBox.M.setHeight(
@@ -108,6 +76,7 @@ export function initExpander(
           ? viewBoxDimensions.M.collapsed
           : viewBoxDimensions.M.expanded
       );
+      observer.next(viewBox.M.getValues());
     }
     if (mediaQuery.L.matches) {
       viewBox.M.setHeight(
@@ -115,6 +84,7 @@ export function initExpander(
           ? viewBoxDimensions.L.collapsed
           : viewBoxDimensions.L.expanded
       );
+      observer.next(viewBox.L.getValues());
     }
     if (mediaQuery.XL.matches) {
       viewBox.M.setHeight(
@@ -122,18 +92,29 @@ export function initExpander(
           ? viewBoxDimensions.XL.collapsed
           : viewBoxDimensions.XL.expanded
       );
+      observer.next(viewBox.XL.getValues());
     }
-  }
-}
+  });
 
-export default function expander(): void {
-  const expanderList: NodeListOf<SVGElement> = document.querySelectorAll(
-    '.js_expander'
-  );
+  const setViewBox = (node: SVGElement): void => {
+    if (node) {
+      viewBoxStream.subscribe((viewbox: string) => {
+        node.setAttribute('viewBox', `${viewbox}`);
+      });
+    }
+  };
+
+  const handleClick = (e: MouseEvent): void => {
+    const target = e.currentTarget as SVGElement;
+    isCollapsed = !isCollapsed;
+
+    viewBoxStream.subscribe((viewbox: string) => {
+      target.setAttribute('viewBox', `${viewbox}`);
+    });
+  };
 
   if (expanderList) {
     [...expanderList].forEach((node: SVGElement) => {
-      initExpander(node);
       setViewBox(node);
       node.addEventListener('click', handleClick);
     });
@@ -142,7 +123,6 @@ export default function expander(): void {
         'resize',
         e => {
           [...expanderList].forEach((node: SVGElement) => {
-            initExpander(node);
             setViewBox(node);
           });
         },
