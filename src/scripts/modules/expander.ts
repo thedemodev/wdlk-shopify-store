74bc0e0b3e1144e14c817fa80755bcfad639e8f2
Mauricio <mauricio.palma@sinnerschrader.com>
2018-09-28 21:07:12 +0200

    feat(viewbox): create a viewbox factory

--- src/scripts/modules/expander.ts
+++ src/scripts/modules/expander.ts
@@ -1,153 +1,89 @@
 import { Observable, Observer } from 'rxjs';
 import { AspectRatio } from './picture-aspect-ratio';
 import { mediaQuery, BreakPoint } from './match-media';
-import { ViewBox } from './viewbox';
-import { ViewBoxDimension } from './viewbox-dimension';
+import { ViewBox, ViewBoxProps } from './viewbox';
 
 export default function expander(): void {
   const viewPortHeight = window.innerHeight;
-  const stickyHeaderHeight: number = (document.querySelector(
-    '.js_sticky-nav'
-  ) as HTMLElement).offsetHeight;
+  const headerHeight = (document.querySelector('.js_sticky-nav') as HTMLElement)
+    .offsetHeight;
   const expanderList: NodeListOf<SVGElement> = document.querySelectorAll(
     '.js_expander'
   );
   if (!expanderList) {
     return;
   }
-  const contentHeightList: number[] = [...expanderList].map((el: Element) => {
-    const elHeight = Math.round(
+
+  const contentHeightList: number[] = [...expanderList].map((el: Element) =>
+    Math.round(
       el.querySelector('.js_expander-content').getBoundingClientRect().height
-    );
-    return elHeight;
-  });
+    )
+  );
   const contentHeight: number = Math.max(...contentHeightList) + 80;
 
-  console.log(contentHeight);
-
+  const viewbox = ViewBox({
+    minX: 0,
+    minY: 0,
+    width: BreakPoint.S,
+    height: BreakPoint.S * AspectRatio.S
+  });
   let isCollapsed = true;
-
-  const viewBox = {
-    S: new ViewBox({
-      minX: 0,
-      minY: 0,
-      width: BreakPoint.S,
-      height: BreakPoint.S * AspectRatio.S
-    }),
-    M: new ViewBox({
-      minX: 0,
-      minY: 0,
-      width: BreakPoint.M,
-      height: BreakPoint.M * AspectRatio.M
-    }),
-    L: new ViewBox({
-      minX: 0,
-      minY: 0,
-      width: BreakPoint.L,
-      height: BreakPoint.L * AspectRatio.L
-    }),
-    XL: new ViewBox({
-      minX: 0,
-      minY: 0,
-      width: BreakPoint.XL,
-      height: BreakPoint.XL * AspectRatio.L
-    })
-  };
-
-  const viewBoxDimensions = {
-    S: ViewBoxDimension({
-      collapsed: {
-        width: BreakPoint.S,
-        height: viewPortHeight / 3 - stickyHeaderHeight / 3
-      },
-      expanded: {
-        width: BreakPoint.S,
-        height: BreakPoint.S * AspectRatio.S + contentHeight
-      }
-    }),
-    M: ViewBoxDimension({
-      collapsed: {
-        width: BreakPoint.M,
-        height: viewPortHeight / 3 - stickyHeaderHeight / 3
-      },
-      expanded: {
-        width: BreakPoint.M,
-        height: BreakPoint.M * AspectRatio.M + contentHeight
-      }
-    }),
-    L: ViewBoxDimension({
-      collapsed: {
-        width: BreakPoint.L * AspectRatio.L,
-        height: BreakPoint.L * AspectRatio.L * 1.5
-      },
-      expanded: {
-        width: BreakPoint.L,
-        height: BreakPoint.L * AspectRatio.L
-      }
-    }),
-    XL: ViewBoxDimension({
-      collapsed: {
-        width: BreakPoint.XL * AspectRatio.L,
-        height: BreakPoint.XL * AspectRatio.L * 1.5
-      },
-      expanded: {
-        width: BreakPoint.L,
-        height: BreakPoint.L * AspectRatio.L
-      }
-    })
-  };
 
   const viewBoxStream = Observable.create((observer: Observer<string>) => {
     if (mediaQuery.S.matches) {
-      viewBox.S.setHeight(
-        isCollapsed
-          ? viewBoxDimensions.S.collapsed.height
-          : viewBoxDimensions.S.expanded.height
-      );
-      observer.next(viewBox.S.getValues());
+      const height = isCollapsed
+        ? viewPortHeight / 3 - headerHeight / 3
+        : BreakPoint.S * AspectRatio.S + contentHeight;
+
+      viewbox.setHeight(height);
     }
+
     if (mediaQuery.M.matches) {
-      viewBox.M.setHeight(
-        isCollapsed
-          ? viewBoxDimensions.M.collapsed.height
-          : viewBoxDimensions.M.expanded.height
-      );
-      observer.next(viewBox.M.getValues());
+      const height = isCollapsed
+        ? viewPortHeight / 3 - headerHeight / 3
+        : BreakPoint.M * AspectRatio.M + contentHeight;
+
+      viewbox.setWidth(BreakPoint.M);
+      viewbox.setHeight(height);
     }
+
     if (mediaQuery.L.matches) {
-      if (isCollapsed) {
-        viewBox.L.setWidth(viewBoxDimensions.L.collapsed.width);
-        viewBox.L.setHeight(viewBoxDimensions.L.collapsed.height);
-      } else {
-        viewBox.L.setWidth(viewBoxDimensions.L.expanded.width);
-        viewBox.L.setHeight(viewBoxDimensions.L.expanded.height);
-      }
-      observer.next(viewBox.L.getValues());
+      const width = isCollapsed ? BreakPoint.L * AspectRatio.L : BreakPoint.L;
+      const height = isCollapsed
+        ? BreakPoint.L * AspectRatio.L * 1.5
+        : BreakPoint.L * AspectRatio.L;
+
+      viewbox.setWidth(width);
+      viewbox.setHeight(height);
     }
+
     if (mediaQuery.XL.matches) {
-      if (isCollapsed) {
-        viewBox.L.setWidth(viewBoxDimensions.L.collapsed.width);
-        viewBox.L.setHeight(viewBoxDimensions.L.collapsed.height);
-      } else {
-        viewBox.L.setWidth(viewBoxDimensions.L.expanded.width);
-        viewBox.L.setHeight(viewBoxDimensions.L.expanded.height);
-      }
-      observer.next(viewBox.XL.getValues());
+      const width = isCollapsed ? BreakPoint.XL * AspectRatio.L : BreakPoint.L;
+      const height = isCollapsed
+        ? BreakPoint.XL * AspectRatio.L * 1.5
+        : BreakPoint.L * AspectRatio.L;
+
+      viewbox.setWidth(BreakPoint.M);
+      viewbox.setHeight(height);
     }
+
+    observer.next(viewbox.getViewBox());
   });
 
   const setViewBox = (node: SVGElement): void => {
-    if (node) {
-      viewBoxStream.subscribe((viewbox: string) => {
-        node.setAttribute('viewBox', `${viewbox}`);
-      });
+    if (!node) {
+      return;
     }
+    viewBoxStream.subscribe((viewboxProps: ViewBoxProps) => {
+      node.setAttribute('viewBox', `${viewboxProps}`);
+    });
   };
 
   const toggleCustomProp = (node: HTMLElement): void => {
-    if (node) {
-      node.style.setProperty('--is-collapsed', `${isCollapsed ? 1 : 0}`);
+    if (!node) {
+      return;
     }
+    node.style.setProperty('--is-collapsed', `${isCollapsed ? 1 : 0}`);
   };
 
   const handleClick = (e: MouseEvent): void => {
@@ -155,8 +91,8 @@ export default function expander(): void {
     const parent = target.parentElement;
     isCollapsed = !isCollapsed;
 
-    viewBoxStream.subscribe((viewbox: string) => {
-      target.setAttribute('viewBox', `${viewbox}`);
+    viewBoxStream.subscribe((viewboxProps: ViewBoxProps) => {
+      target.setAttribute('viewBox', `${viewboxProps}`);
       toggleCustomProp(parent);
     });
   };
--- src/scripts/modules/viewbox.ts
+++ src/scripts/modules/viewbox.ts
@@ -1,31 +1,42 @@
-interface ViewBoxInit {
+export interface ViewBoxProps {
   minX: number;
   minY: number;
   width: number;
   height: number;
 }
 
-export class ViewBox {
-  private minX: number;
-  private minY: number;
-  private width: number;
-  private height: number;
+export interface ViewBoxFactoryProps {
+  minX: number;
+  minY: number;
+  width: number;
+  height: number;
+  getWidth(): number;
+  getHeight(): number;
+  getViewBox(): string;
+  setWidth(width: number): void;
+  setHeight(height: number): void;
+}
 
-  public constructor(init: ViewBoxInit) {
-    this.minX = init.minX;
-    this.minY = init.minY;
-    this.width = init.width;
-    this.height = init.height;
-  }
-
-  public setWidth(width: number): void {
-    this.width = width;
-  }
-  public setHeight(height: number): void {
-    this.height = height;
-  }
-
-  public getValues(): string {
-    return `${this.minX} ${this.minY} ${this.width} ${this.height}`;
-  }
+export function ViewBox(init: ViewBoxProps): ViewBoxFactoryProps {
+  return {
+    minX: init.minX,
+    minY: init.minY,
+    width: init.width,
+    height: init.height,
+    getWidth(): number {
+      return this.width;
+    },
+    getHeight(): number {
+      return this.height;
+    },
+    getViewBox(): string {
+      return `${this.minX} ${this.minY} ${this.width} ${this.height}`;
+    },
+    setWidth(width: number): void {
+      this.width = width;
+    },
+    setHeight(height: number): void {
+      this.height = height;
+    }
+  };
 }
