a61d6959b2ba1623808a6fffd572367cd207a984
Mauricio Palma <mauricio.palma@sinnerschrader.com>
2019-06-23 16:22:46 +0200

    feat(slider): restock banner (#40)
    * feat(slider): include new banner and fix slider init
    
    * feat(slider): adjust styling for the main banners

--- src/scripts/home/home.ts
+++ src/scripts/home/home.ts
@@ -3,13 +3,12 @@ import * as Slider from '../slider';
 import * as Types from '../types';
 
 const sliderEl = document.querySelector('.js_slider');
-const configuration: Types.SliderInit = {
+
+Slider.init({
   sliderEl,
   trackEl: sliderEl.querySelector('.js_track'),
   config: Slider.Config.create({ transitionDuration: 300 })
-};
-
-Slider.init(configuration);
+});
 
 IG.create({
   mountEl: document.querySelector('.js_instagramFeed'),
